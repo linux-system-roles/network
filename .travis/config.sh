5d7e5441348de9191890c6347009717be40dee49
Jiri Kucera <jkucera@redhat.com>
2019-07-19 11:45:26 +0200

    Allow running tox also locally
    - use `./.travis/runtox [ARG1 [ARG2 [...[ARGn]]]]` to run tox locally
    - use LSR_USE_PYTHON to decide whether to run jobs with pylinters and
      pytesters or not

---
+++ .travis/config.sh
@@ -0,0 +1,14 @@
+export LSR_PYLINT_DIRS='library/network_connections.py module_utils/network_lsr tests/unit/test_network_connections.py'
+export LSR_MOLECULE_DEPS='-rmolecule_requirements.txt'
+
+case "x${TRAVIS_PYTHON_VERSION}" in
+  x3.5)
+    LSR_EXTRA_PACKAGES='python3-selinux'
+    ;;
+  x3.6|x)
+    # Set these also if we are running locally:
+    export LSR_TEXTRA_DEPS='PyYAML'
+    export LSR_TEXTRA_DIR='tests'
+    export LSR_TEXTRA_CMD='./ensure_non_running_provider.py'
+    ;;
+esac
