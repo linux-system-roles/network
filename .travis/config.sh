0ae8076e55c694eaa5f031f298b829e8d337a411
Jiri Kucera <jkucera@redhat.com>
2019-09-13 14:12:44 +0200

    Install extra packages allways
    Do not make extra package installing dependent on TRAVIS_PYTHON_VERSION.
    Instead, install them if they are available (silently ignore errors
    about missing packages).

--- .travis/config.sh
+++ .travis/config.sh
@@ -1,10 +1,9 @@
 # SPDX-License-Identifier: MIT
 export LSR_MOLECULE_DEPS='-rmolecule_requirements.txt'
 
+LSR_EXTRA_PACKAGES='python3-selinux'
+
 case "${TRAVIS_PYTHON_VERSION}" in
-  3.5)
-    LSR_EXTRA_PACKAGES='python3-selinux'
-    ;;
   3.6|"")
     # Set these also if we are running locally:
     export LSR_TEXTRA_DEPS='PyYAML'
