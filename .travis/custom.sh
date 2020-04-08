c2b2c94e55329c0a72ab7f97f05f071806befdc1
Till Maas <opensource@till.name>
2020-04-08 11:52:52 +0200

    config.sh/custom.sh: Sync with template

--- .travis/custom.sh
+++ .travis/custom.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 # SPDX-License-Identifier: MIT
 
-# This script is executed with two arguments passed to it:
-#
-#   $1 - path to environment python (python used inside virtual environment
-#        created by tox)
-#   $2 - path to system python (python 3.x installed on the system)
-
 set -e
 
 ME=$(basename $0)
 SCRIPTDIR=$(readlink -f $(dirname $0))
-TOPDIR=$(readlink -f ${SCRIPTDIR}/..)
 
 . ${SCRIPTDIR}/utils.sh
 . ${SCRIPTDIR}/config.sh
