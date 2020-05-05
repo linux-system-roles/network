b4084b9a41506b9bdcb201ba80f55b9a7084b917
Till Maas <opensource@till.name>
2020-05-05 14:59:12 +0200

    Travis: Fix python2 detection

--- .travis/config.sh
+++ .travis/config.sh
@@ -39,7 +39,7 @@
 #
 #       - RUN_FLAKE8_DISABLED
 #       - RUN_FLAKE8_EXTRA_ARGS
-if [[ "$(python2 -c "import sys; print(sys.version_info.major)")" == "2" ]]
+if [[ "$(python -c "import sys; print(sys.version_info.major)")" == "2" ]]
 then
     PYTHON2_EXCLUDES="tests/ensure_provider_tests.py"
     FLAKE8_DEFAULT_EXCLUDES=".svn,CVS,.bzr,.hg,.git,__pycache__,.tox,.eggs,*.egg"
