6a73bd9bc45e59c023bcbbc21c73e2bdb6fb7c2f
Rich Megginson <rmeggins@redhat.com>
2020-11-04 10:24:31 -0700

    make CI work with tox-lsr
    Use tox-lsr instead of the scripts in the `.travis/` directory and the
    asssociated config files.  Yes, `.travis/custom.sh` is still there,
    but once you are familiar with the way that tox-lsr works, you can
    remove it and use your own custom tox.ini action.  The goal of this
    commit was to make as few changes as possible, to keep feature parity
    with the old `.travis/` script way of running CI.  Also note that
    linting has moved into tox, so we needed to disable molecule linting.
    The new version of ansible-lint adds a lot of checks, so these are
    disabled for now, and should be fixed later.  Also, shellcheck is
    turned off until the network team can address the issues.
    
    The `.travis/custom.sh` script is not run in the CI environment, so
    this was changed slightly to remove the check for `$TRAVIS`, and it is
    no longer run as a tox testenv in CI.
    
    Signed-off-by: Rich Megginson <rmeggins@redhat.com>

--- .travis/custom.sh
+++ .travis/custom.sh
@@ -3,13 +3,9 @@
 
 set -e
 
-ME=$(basename $0)
-SCRIPTDIR=$(readlink -f $(dirname $0))
-
-. ${SCRIPTDIR}/utils.sh
-. ${SCRIPTDIR}/config.sh
+. "$LSR_SCRIPTDIR/utils.sh"
 
 # Write your custom commands here that should be run when `tox -e custom`:
-if [[ -z "${TRAVIS}" ]] || lsr_check_python_version python -eq '3.6'; then
-    (set -x; cd ${TOPDIR}/tests; ${ENVPYTHON} ./ensure_provider_tests.py)
+if lsr_check_python_version python -eq '3.6'; then
+    (set -x; cd "${TOPDIR}/tests"; python ./ensure_provider_tests.py)
 fi
