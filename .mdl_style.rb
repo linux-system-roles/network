9e427428b452fdeea685a4530f2b0eb2520fc55f
Wen Liang <liangwen12year@gmail.com>
2021-04-07 14:33:04 -0400

    Add markdownlint rules to allow header duplication
    Markdownlint requires each section headers to be different by default
    but the Changelog needs to contain multiple sections with the same
    name, therefore enable `allow_different_nesting` to support this use
    case.
    
    Signed-off-by: Wen Liang <liangwen12year@gmail.com>

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -5,3 +5,5 @@ rule 'MD003', :style => :setext_with_atx
 rule 'MD013', :line_length => 88
 # https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md#md029---ordered-list-item-prefix
 rule 'MD029', :style => :ordered
+# https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md#md024---multiple-headers-with-the-same-content
+rule "MD024", :allow_different_nesting => true
