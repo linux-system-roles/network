c40ede89f4f15453dbae0099ce6034dcc032ddb8
Till Maas <opensource@till.name>
2023-02-09 14:27:00 +0100

    mdl_style: Fix rule URLs to use main branch
    Signed-off-by: Till Maas <opensource@till.name>

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -1,9 +1,9 @@
 all
-# https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md#md003---header-style
+# https://github.com/markdownlint/markdownlint/blob/main/docs/RULES.md#md003---header-style
 rule 'MD003', :style => :setext_with_atx
-# https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md#md013---line-length
+# https://github.com/markdownlint/markdownlint/blob/main/docs/RULES.md#md013---line-length
 rule 'MD013', :line_length => 88
-# https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md#md029---ordered-list-item-prefix
+# https://github.com/markdownlint/markdownlint/blob/main/docs/RULES.md#md029---ordered-list-item-prefix
 rule 'MD029', :style => :ordered
-# https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md#md024---multiple-headers-with-the-same-content
+# https://github.com/markdownlint/markdownlint/blob/main/docs/RULES.md#md024---multiple-headers-with-the-same-content
 rule "MD024", :allow_different_nesting => true
